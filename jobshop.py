ebea91dad2d81ab0b60c410c96b688ba54dd67c6
Marc Light <gaulight42@gmail.com>
2016-07-31 20:55:37 +0000

    added in YAML processing for Machines

--- jobshop.py
+++ jobshop.py
@@ -4,35 +4,35 @@ import yaml
 class Task:
     """Hold information about a task"""
     def __init__(self):
-        coresRequired = 0
-        executionTime = 0
-        parentTasks = ()
-        childTasks = ()
-        maxPath = 0
-        readyToRun = 0
-        startTime = 0
-        finishTime = 0
+        self.coresRequired = 0
+        self.executionTime = 0
+        self.parentTasks = ()
+        self.childTasks = ()
+        self.maxPath = 0
+        self.readyToRun = 0
+        self.startTime = 0
+        self.finishTime = 0
 
     def leafP(self):
-        return childTasks
+        return self.childTasks
 
     def rootP(self):
-        return parentTasks
+        return self.parentTasks
 
 class Machine:
     """Hold information about a machine"""
-    def __init__(self,c):
-        name = ""
-        cores = c
-        coresAvailable = 0
+    def __init__(self,n,c):
+        self.name = n
+        self.cores = c
+        self.coresAvailable = self.cores
 
 class SchedState:
     """Hold information about a state where a schedule is a list of such states"""
     def __init__(self):
-        time = 0
-        priorityList = [] #tasks ordered by priority based on critical path algorithm
-        tasksRunning = [] # list of tasks sorted by finishtime
-        machines     = [] # list of Machine objects sorted by coresAvailable in ascending order
+        self.time = 0
+        self.priorityList = [] #tasks ordered by priority based on critical path algorithm
+        self.tasksRunning = [] # list of tasks sorted by finishtime
+        self.machines     = [] # list of Machine objects sorted by coresAvailable in ascending order
 
     def setupStartState(self,priorityList,machines):
         pass
@@ -45,24 +45,30 @@ class Scheduler:
     """Outer class that holds all the bits of the problem, intermediate
     states, and relevant functions"""
     def __init__(self):
-        machines = []
-        tasks = []
-        Schedule = [] # list of (task,startTime,endTime,machine,cores)
+        self.machines = []
+        self.tasks = []
+        self.Schedule = [] # list of (task,startTime,endTime,machine,cores)
 
     def schedule(self,tasksfile,machinesfile):
         """Outermost function that reads in task and machines YAML files and
         then fires off the scheduling algorithm"""
-        with open(tasksfile, 'r') as stream:
+        self.machines = self.createMachines(machinesfile)
+
+
+    def createMachines(self,filename):
+        with open(filename, 'r') as stream:
             try:
-             print(yaml.load(stream))
+                mData = yaml.load(stream)
+                return [Machine(n,c) for n, c in mData.items()]
             except yaml.YAMLError as exc:
-               print(exc)
+                print(exc)
 
 
 
 
 if __name__ == "__main__":
     s = Scheduler()
-    s.schedule("jobshop/examples/task1.yaml","jobshop/examples/machine1.yaml")
+    s.schedule("jobshop/examples/task1.yaml","jobshop/examples/machines1.yaml")
+    print ["%s-%s" % (m.name,m.cores) for m in s.machines]
 
 
