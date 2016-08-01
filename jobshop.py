012ff6506fa4b1ad69ea48b0f2ae21b06dedf4d5
Marc Light <gaulight42@gmail.com>
2016-08-01 02:50:43 +0000

    set up all the tasks

--- jobshop.py
+++ jobshop.py
@@ -3,11 +3,12 @@ import yaml
 
 class Task:
     """Hold information about a task"""
-    def __init__(self):
-        self.coresRequired = 0
-        self.executionTime = 0
-        self.parentTasks = ()
-        self.childTasks = ()
+    def __init__(self,n,coresR,eTime,pTasks):
+        self.name = n
+        self.coresRequired = coresR
+        self.executionTime = eTime
+        self.parentTasks   = pTasks # a list of strings
+        self.childTasks = [] # a list of Task objects
         self.maxPath = 0
         self.readyToRun = 0
         self.startTime = 0
@@ -19,12 +20,19 @@ class Task:
     def rootP(self):
         return self.parentTasks
 
+    def __repr__(self):
+        return "<Task %s, coresR %s, eTime %s, pTasks %s, cTasks %s>" % (self.name,self.coresRequired,self.executionTime,[t.name for t in self.parentTasks],[t.name for t in self.childTasks])
+
+
 class Machine:
     """Hold information about a machine"""
     def __init__(self,n,c):
         self.name = n
         self.cores = c
         self.coresAvailable = self.cores
+
+    def __repr__(self):
+        return "<Machine: name: %s, cores: %s, available %s>" % (self.name, self.cores, self.coresAvailable)
 
 class SchedState:
     """Hold information about a state where a schedule is a list of such states"""
@@ -47,15 +55,20 @@ class Scheduler:
     def __init__(self):
         self.machines = []
         self.tasks = []
+        self.tasksDict = {} # enable fast access based on name
         self.Schedule = [] # list of (task,startTime,endTime,machine,cores)
 
     def schedule(self,tasksfile,machinesfile):
         """Outermost function that reads in task and machines YAML files and
         then fires off the scheduling algorithm"""
         self.machines = self.createMachines(machinesfile)
+        self.machines.sort(key=lambda m: m.coresAvailable)
+
+        self.tasks = self.createTasks(tasksfile)
 
 
     def createMachines(self,filename):
+        """create machines from YAML file to be resources for a schedule"""
         with open(filename, 'r') as stream:
             try:
                 mData = yaml.load(stream)
@@ -63,12 +76,37 @@ class Scheduler:
             except yaml.YAMLError as exc:
                 print(exc)
 
+    def createTasks(self,filename):
+        """create tasks from YAML file to be scheduled"""
+        with open(filename, 'r') as stream:
+            try:
+                tData = yaml.load(stream)
+                ts = [self.cTask(n,attrs) for n,attrs in tData.items()]
+                self.createChildrenPointers(ts)
+                return ts
+            except yaml.YAMLError as exc:
+                print(exc)
+    def cTask(self,name,ats):
+        """helper function of createTasks: does one task"""
+        t = Task(name,ats['cores_required'],ats['execution_time'],ats['parent_tasks'].keys())
+        self.tasksDict[name] = t
+        return t
+    def createChildrenPointers(self,tasks):
+        """file in the children field for each task"""
+        for t in tasks:
+            for p in t.parentTasks:
+                self.tasksDict[p].childTasks.append(t)
+            newParents = []
+            for p in t.parentTasks:
+                newParents.append(self.tasksDict[p])
+            t.parentTasks = newParents
 
 
 
 if __name__ == "__main__":
     s = Scheduler()
     s.schedule("jobshop/examples/task1.yaml","jobshop/examples/machines1.yaml")
-    print ["%s-%s" % (m.name,m.cores) for m in s.machines]
+    print s.machines
+    print s.tasks
 
 
